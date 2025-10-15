50c33fe9c0adf7f4f14be9d45a684cce2ff10727
Shreyas-satish-07 <shreyas.sdvg07@gmail.com>
2025-10-15 11:50:31 +0530

    added files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Banner from './components/banner'
+
+function App() {
+  return (
+    <>
+    < Navbar /> 
+    <Banner />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <>
+    <div className='max-w-screen-2xl mx-auto md:px-20 px-4'>
+        <div></div>
+        <div></div>
+    </div>
+    </>
+  )
+}
+
+export default Banner
